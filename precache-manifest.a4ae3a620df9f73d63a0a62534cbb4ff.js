5963de3e2ac935b5abbda1c01a4d9625aeaebf61
Dyarlen Iber <dyarlen1@gmail.com>
2020-06-16 18:54:09 +0100

    Updates

---
+++ precache-manifest.a4ae3a620df9f73d63a0a62534cbb4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaee93495b5d3f7abea22e5b4a38e098",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "d1d95381d5862147d695",
+    "url": "/react-to-do-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6fa588337ab49ad9df2d",
+    "url": "/react-to-do-list/static/css/main.b175d387.chunk.css"
+  },
+  {
+    "revision": "d1d95381d5862147d695",
+    "url": "/react-to-do-list/static/js/2.577463fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-to-do-list/static/js/2.577463fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa588337ab49ad9df2d",
+    "url": "/react-to-do-list/static/js/main.45d464ae.chunk.js"
+  },
+  {
+    "revision": "c73471039189803cfd80",
+    "url": "/react-to-do-list/static/js/runtime-main.8a67d964.js"
+  }
+]);
